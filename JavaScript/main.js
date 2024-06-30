5d1ad828359954311978339233fe4f2f9652f41f
OxO <xxsharutoxx@gmail.com>
2024-06-30 22:37:55 +0900

    first commit

---
+++ JavaScript/main.js
@@ -0,0 +1,154 @@
+"use strict";
+
+const FONT = "48px monospace"
+const WIDTH = 128;      // 仮想画面の幅
+const HEIGHT = 120;     // 仮想画面の高さ
+const MAP_WIDTH = 32;   // マップの幅
+const MAP_HEIGHT = 32;  // マップの高さ
+const SMOOTH = 0;       // 補完処理
+const TILESIZE = 8;     // タイルサイズ
+const TILECOLUMN = 4;   // タイル列数
+const TILEROW = 4;      // タイル行数
+const CHRHEIGHT = 9;
+const CHRWIDTH = 8;
+
+let gScreen;        // 仮想画面
+let gFrame = 0;     // 内部カウンタ
+let gHeight;        // 実画面の高さ
+let gWidth;         // 実画面の幅
+let gImgMap;        // 画像（マップ）
+let gImgPlayer;     // 画像（プレイヤー）
+let gPlayerX = 10;   // プレイヤーのX座標
+let gPlayerY = 5;   // プレイヤーのY座標
+
+
+const gFileMap      = "img/map.png";
+const gFilePlayer   = "img/player.png";
+
+// マップ
+const	gMap = [
+    0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+    0, 0, 3, 3, 3, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+    0, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 0, 3, 0, 3, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+    0, 3, 3, 7, 7, 7, 7, 7, 7, 7, 7, 7, 6, 6, 3, 6, 3, 6, 3, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+    0, 3, 3, 6, 6, 7, 7, 7, 2, 2, 2, 7, 7, 7, 7, 7, 7, 7, 6, 3, 0, 0, 0, 3, 3, 0, 6, 6, 6, 0, 0, 0,
+    0, 0, 3, 3, 6, 6, 6, 7, 7, 2, 2, 2, 7, 7, 2, 2, 2, 7, 7, 6, 3, 3, 3, 6, 6, 3, 6,13, 6, 0, 0, 0,
+    0, 3, 3,10,11, 3, 3, 6, 7, 7, 2, 2, 2, 2, 2, 2, 1, 1, 7, 6, 6, 6, 6, 6, 3, 0, 6, 6, 6, 0, 0, 0,
+    0, 0, 3, 3, 3, 0, 3, 3, 3, 7, 7, 2, 2, 2, 2, 7, 7, 1, 1, 6, 6, 6, 6, 3, 0, 0, 0, 0, 0, 0, 0, 0,
+    0, 0, 0, 0, 0, 0, 0, 0, 3, 3, 7, 7, 7, 7, 2, 7, 6, 3, 1, 3, 6, 6, 6, 3, 0, 0, 0, 3, 3, 0, 0, 0,
+    0, 0, 0, 0, 0, 0, 0, 0, 0, 3, 3, 6, 6, 7, 2, 7, 6, 3, 1, 3, 3, 6, 6, 3, 0, 0, 0, 3, 3, 0, 0, 0,
+    0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 3, 3, 6, 7, 7, 7, 6, 3, 1, 1, 3, 3, 6, 3, 3, 0, 0, 3, 3, 3, 0, 0,
+    0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 3, 6, 6, 7, 7, 7, 6, 3, 1, 1, 3, 3, 6, 3, 3, 0, 3,12, 3, 0, 0,
+    0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 3, 3, 6, 6, 6, 7, 7, 6, 3, 1, 1, 3, 3, 3, 3, 3, 3, 3, 3, 0, 0,
+    0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 3, 3, 3, 3, 6, 6, 6, 6, 3, 1, 1, 1, 1, 3, 3, 3, 3, 3, 3, 0,
+    0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 3, 3, 3, 3, 3, 3, 6, 6, 3, 3, 3, 3, 1, 1, 3, 3, 3, 1, 1, 0,
+    0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 3, 4, 5, 3, 3, 3, 6, 6, 6, 3, 3, 3, 1, 1, 1, 1, 1, 3, 0,
+    0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 3, 8, 9, 3, 3, 3, 6, 6, 6, 6, 3, 3, 3, 3, 3, 3, 1, 0, 0,
+    0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 3, 3, 3, 3, 3, 3, 3, 6, 6, 6, 3, 3, 3, 3, 3, 3, 0, 0, 0,
+    0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 3, 3, 3, 3, 3, 3, 3, 6, 6, 6, 6, 3, 3, 3, 3, 0, 0, 0, 0,
+    0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 3, 3, 3, 3, 3, 3, 3, 6, 6, 6, 6, 3, 3, 3, 0, 0, 0, 0, 0,
+    0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 3, 3, 3, 3, 3, 3, 3, 6, 6, 6, 3, 3, 3, 0, 0, 0, 0, 0, 0,
+    0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 3, 3, 6, 6, 6, 3, 6, 6, 6, 3, 3, 3, 0, 0, 0, 0, 0, 0, 0,
+    0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 3, 3, 6, 6, 3, 6, 6, 3, 3, 3, 0, 0, 0, 0, 0, 0, 0, 0,
+    0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 3, 3, 6, 6, 3, 6, 6, 3, 3, 3, 0, 0, 0, 0, 0, 0, 0, 0,
+    0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 3, 3, 6, 3, 0, 0, 0, 0, 3, 0, 0, 0, 0, 0, 0, 0, 0,
+    0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 3, 6, 3, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+    0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 6, 3, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+    0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,14, 6, 0, 0, 6, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+    0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 6, 6, 6, 6, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+    0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 6, 6, 7, 0, 0, 0, 0, 0, 0, 0, 0,
+    7,15, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 7, 7, 7, 7, 0, 0, 0, 0, 0,
+    7, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 7, 7, 7, 7, 7, 7,
+];
+   
+   
+function DrawMain()
+{
+    const g = gScreen.getContext("2d");              // 仮想画面の2D描画コンテキストを取得
+
+    for (let y=0; y<20; y++){
+        for (let x=0; x<20; x++){
+            let px = gPlayerX + x;
+            let py = gPlayerY + y;
+            DrawTile(g, x*TILESIZE, y*TILESIZE, gMap[py*MAP_WIDTH + px])
+        }
+    }
+
+    g.drawImage(gImgPlayer, 
+                8, 0, CHRWIDTH, CHRHEIGHT,
+                WIDTH / 2 , HEIGHT / 2, CHRWIDTH, CHRHEIGHT);
+}
+
+function DrawTile(g, x, y, idx)
+{
+    const ix = (idx % TILECOLUMN) * TILESIZE;
+    const iy = Math.floor(idx / TILECOLUMN) * TILESIZE;
+    g.drawImage(gImgMap, ix, iy, TILESIZE, TILESIZE, x, y, TILESIZE, TILESIZE);
+}
+
+function LoadImage()
+{
+    gImgMap     = new Image(); gImgMap.src      = gFileMap;    // マップ画像読み込み
+    gImgPlayer  = new Image(); gImgPlayer.src   = gFilePlayer; // プレイヤー画像読み込み
+}
+
+function WmPaint()
+{
+    DrawMain();
+
+    const ca = document.getElementById("main"); // mainキャンバスの要素を取得
+    const g = ca.getContext("2d");              // 2D描画コンテキストを取得
+
+    g.drawImage(gScreen, 0, 0, gScreen.width, gScreen.height, 0, 0, gWidth, gHeight);
+}
+
+// ブラウザサイズ変更イベント
+function WmSize()
+{
+    const ca = document.getElementById("main"); // mainキャンバスの要素を取得
+    ca.width = window.innerWidth;               // キャンバスの幅をブラウザの幅へ変更
+    ca.height = window.innerHeight;             // キャンバスの高さをブラウザの高さへ変更
+
+    const g = ca.getContext("2d");              // 2D描画コンテキストを取得
+    g.imageSmoothingEnabled = g.msImageSmoothingEnabled = SMOOTH;
+
+    // 実画面サイズを計測
+    gWidth = ca.width;
+    gHeight = ca.height;
+    if (gWidth / WIDTH < gHeight / HEIGHT){
+        gHeight = gWidth * HEIGHT / WIDTH;
+    } else{
+        gWidth = gHeight* WIDTH / HEIGHT;
+    }
+}
+
+// タイマーイベント発生時の処理
+function WmTimer()
+{
+    gFrame++;
+    WmPaint();
+}
+
+// キー入力イベント
+window.onkeydown = function(ev)
+{
+    let c = ev.keyCode;     // キーコード取得
+    if(c == 37) gPlayerX--; // 左
+    if(c == 38) gPlayerY--; // 上
+    if(c == 39) gPlayerX++; // 右
+    if(c == 40) gPlayerY++; // 下
+}
+
+// ブラウザ起動イベント
+window.onload = function()
+{
+    LoadImage();
+
+    gScreen = document.createElement("canvas");     // 仮想画面を作成
+    gScreen.width = WIDTH;                          
+    gScreen.height = HEIGHT;
+
+    WmSize();                                                   // 画面サイズ初期化
+    window.addEventListener("resize", function(){WmSize()});    // ブラウザサイズ変更時WmSize()が呼ばれる
+    setInterval(function(){WmTimer()}, 33);                     // 33ms感覚で，WmTimer()を呼び出すように指示
+}
